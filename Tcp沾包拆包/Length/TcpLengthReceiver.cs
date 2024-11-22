a42ea02d5f96194507c24c8c1b400611cf04bf06
ldh <1152417278@qq.com>
2024-11-22 16:22:04 +0800

    消息长度法

---
+++ Tcp沾包拆包/Length/TcpLengthReceiver.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpDipAndUnpack.Length
+{
+    public class TcpLengthReceiver
+    {
+        private const string ServerIP = "127.0.0.1";
+        private const int ServerPort = 8890;
+
+        public static void ReceiveDataWithLength()
+        {
+            try
+            {
+                // 创建TCP套接字并监听端口
+                TcpListener server = new TcpListener(IPAddress.Parse(ServerIP), ServerPort);
+                server.Start();
+
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                    byte[] lengthData = new byte[4];
+                    int bytesRead = stream.Read(lengthData, 0, 4);
+                    if (bytesRead == 0)
+                    {
+                        break;
+                    }
+
+                    int length = BitConverter.ToInt32(lengthData, 0);
+                    byte[] data = new byte[length];
+                    int totalBytesRead = 0;
+                    while (totalBytesRead < length)
+                    {
+                        int bytesThisRead = stream.Read(data, totalBytesRead, length - totalBytesRead);
+                        if (bytesThisRead == 0)
+                        {
+                            break;
+                        }
+
+                        totalBytesRead += bytesThisRead;
+                    }
+
+                    if (totalBytesRead == length)
+                    {
+                        string receivedData = Encoding.ASCII.GetString(data);
+                        Console.WriteLine($"Received: {receivedData}");
+                    }
+                }
+
+                stream.Close();
+                client.Close();
+                server.Stop();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"接收数据时出错: {e.Message}");
+            }
+        }
+    }
+}
---
+++ Tcp沾包拆包/Length/TcpLengthSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpDipAndUnpack.Length
+{
+    internal class TcpLengthSender
+    {
+        private const string ServerIP = "127.0.0.1";
+        private const int ServerPort = 8890;
+
+        public static void SendDataWithLength()
+        {
+            try
+            {
+                // 创建TCP套接字
+                TcpClient client = new TcpClient();
+                client.Connect(ServerIP, ServerPort);
+
+                NetworkStream stream = client.GetStream();
+                string[] messages = { "message1", "message2", "message3" };
+
+                foreach (string msg in messages)
+                {
+                    int length = msg.Length;
+                    byte[] lengthData = BitConverter.GetBytes(length);
+                    stream.Write(lengthData, 0, lengthData.Length);
+                    byte[] data = Encoding.ASCII.GetBytes(msg);
+                    stream.Write(data, 0, data.Length);
+                }
+
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"发送数据时出错: {e.Message}");
+            }
+        }
+    }
+}
